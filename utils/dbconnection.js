dacb7ce4f8fea1b46aaaaf0b7e9927e7f63bc872
Deepak Kumar <dk4503835@gmail.com>
2025-07-25 16:52:33 +0530

    feat: connection code Added

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -1,3 +1,19 @@
+ import { config } from "dotenv";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+ 
+  dotenv.config();
+// Connect to MongoDB
+ const dbConnection = async () => {
+    mongoose
+    .connect(process.env.MONGO_URL) 
+    .then(()=>{
+        console.log("Database connected successfully");
+    })
+    .catch((error) => {
+        console.log("Database connection failed", error);
+        
+    })
 
-
-     
+ }
+ export default dbConnection;
