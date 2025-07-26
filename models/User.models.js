1882fbcf35b8726a35eb12cd388435b82b34feb5
Deepak Kumar <dk4503835@gmail.com>
2025-07-26 19:19:24 +0530

    feat(user): add password hashing middleware

--- models/User.models.js
+++ models/User.models.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema({
    
     firstName: {
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
         trim : true, 
     
     },
-    lastname:{
+    lastName:{
         type: String,
         required: true,
         trim: true,
@@ -52,11 +52,29 @@ const userSchema = new mongoose.Schema({
     // },
 
 },
+
 {
     timestamps: true, // ye hame createdAt aur updatedAt fields automatically add kar deta hai
 }
 
-)
+);
+// Save hone se pehle pre-hook run hoga,
+// this ks referance current document(user)hota hai
+//isModified("Password")check karte hai ki password field koi naya value set hua hai ya nhi
+//Agar user ka password same hai (update me nahi badla), toh fir se hash nahi karega.
+//Performance ke liye important hai, warna har save me password dobara hash ho jayega.
+ userSchema.pre("save" ,async function(next){
+    if(this.isModified("password")){
+        // hashing me conver hoga 10 ye batata hai ki kitani complexity use hogi
+        //hashing ke liye
+        this.password = await bcrypt.hash(this.password,10)
+    
+    }
+    next();
+
+ });
+
+
 // ye hame user ka model banane me help karata hai
   const User = mongoose.model("User", userSchema);
 
