58f67202c16808d2f07e4b77a5e335adcbb8a4cb
Deepak Kumar <dk4503835@gmail.com>
2025-07-28 09:44:03 +0530

    feat(routes): add routes and controllers

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { registerUser, verifyUser,login } from "../controller/user.controller.js";
+import {
+  registerUser,
+  verifyUser,
+  login,
+  Profile,
+  logoutUser,
+  forgotPassword,
+} from "../controller/user.controller.js";
+import { isLoggedIn } from "../middleware/auth.middleware.js";
 
 
 
@@ -8,6 +16,9 @@ const router = express.Router();
 router.post("/register",registerUser);
 router.get("/verify:token", verifyUser);
 router.post("/login", login);
+router.get("/profile", isLoggedIn, Profile);
+router.post("/logout", isLoggedIn, logoutUser);
+router.post("/forgotPassword", isLoggedIn, forgotPassword);
 
 
 export default router;
