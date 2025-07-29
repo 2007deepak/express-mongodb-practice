906512d478146f67dd24cfd19d6cc261dd29fab2
Deepak Kumar <dk4503835@gmail.com>
2025-07-29 09:37:32 +0530

    feat(auth): add login API with bcrypt password check and JWT session handling

--- FULLSTACKSQL/controllers/auth.controller.js
+++ FULLSTACKSQL/controllers/auth.controller.js
@@ -1,6 +1,8 @@
-import bcrypt from "bcryptjs"
+import bcrypt, { compare } from "bcryptjs"
 import crypto from "crypto"
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
+import { hasSubscribers } from "diagnostics_channel";
 
 const prisma = new PrismaClient();
 
@@ -61,3 +63,79 @@ export const registerUser = async (req , res) => {
 
 };
 
+export  const login = async(req,res) => {
+
+    //fetch data 
+    const {password,email} = req.body;
+
+    //validation
+     if(!password || !email){
+        return res.status(400).json({
+            success:false,
+            message: "All field are required",
+        })
+     }
+    // if user exist
+      try {
+          const user =  await prisma.user.findUnique({
+          where: {email}
+        })
+        
+        if(!user){
+          return res.status(400).json({
+              success: false,
+              message: "User not found",
+          });
+        }
+      // match the password
+        const isMatch = await bcrypt.compare(password ,user.password)
+        console.log(isMatch);
+
+        if(!isMatch){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password",
+            });
+        }
+       // ab yadi password match kar gya to kya karna hai
+       // hame session create karana hai by jwt
+        
+        const token = jwt.sign({
+              id: user._id, role: user.role },
+              
+             process.env.JWT_SECRET,
+               {
+                expiresIn : "24h"
+              }
+            );
+            const cookieOption = {
+              //ye karne se cokies backend ke haath me chala jataa hai user ke conroll me 9nhi 
+              //rahta hai 
+              httpOnly : true,
+             // secure: true,
+             // maxAge: 24*60*60*1000
+            }
+            res.cookie("token",token, cookieOption)
+            
+              res.status(200).json({
+                success: true,
+                token,
+                user: {
+                  id: user._id,
+                  name: user.firstName,
+                  role: user.role,
+                  phone:user.phone,
+                },
+                  message: "Login successful",
+              });
+        
+  
+      } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message:"Login failed",
+        });
+        
+      }
+
+}
